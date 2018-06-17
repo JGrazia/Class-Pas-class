8781026da3e1d37fb118123f961f224c156518a2
U-DESKTOP-RPO5CNM\Elendils <juliengraziani@hotmail.fr>
2018-06-17 20:59:27 +0200

    commit

---
+++ index.js
@@ -0,0 +1,21 @@
+$(document).ready(function()
+{
+    $(".button1").click(function()
+    {
+        $(".grp-carre, .grp-cercle, .losange").addClass("blue");
+    });
+});
+
+$(document).ready(function()
+{
+    $(".button2").click(function()
+    {
+        $(".grp-carre, .grp-cercle, .losange").removeClass("blue");
+    });
+});
+
+
+	$(".grp-cercle").click(function()
+    {
+        $(this).toggleClass("blue");
+    });
